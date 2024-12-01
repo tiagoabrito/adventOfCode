a6b696ab56db5d345e0d77d5776ce4ff4fa14370
Tiago Brito (CTW) <tiago.brito@ctw.bmwgroup.com>
2024-12-01 10:36:20 +0000

    AOC: 2024 day 1

--- src/year2024/day01/Day.kt
+++ src/year2024/day01/Day.kt
@@ -1,27 +1,33 @@
 package year2024.day01
 
+import kotlin.math.abs
 import year2024.solveIt
 
 
 fun main() {
     val day = "01"
-    val expectedTest1 = 0L
-    val expectedTest2 = 0L
+    val expectedTest1 = 11L
+    val expectedTest2 = 31L
 
 
     fun part1(input: List<String>): Long {
-        return 0
+        val map = input.map { it.split("   ") }
+        val listA = map.map { it[0].toLong() }.sorted()
+        val listB = map.map { it[1].toLong() }.sorted()
+        return listA.zip(listB).sumOf { (a, b) -> abs(a - b) }
     }
 
 
     fun part2(input: List<String>): Long {
-        return 0
+        val map = input.map { it.split("   ") }
+        val listA = map.map { it[0].toLong() }.sorted()
+        val occurrencesCount = map.map { it[1].toLong() }.groupingBy { it }.eachCount()
 
-
+        return listA.sumOf { it * (occurrencesCount[it] ?: 0) }
     }
 
 
-    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test2")
+    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test")
 
 }
 
