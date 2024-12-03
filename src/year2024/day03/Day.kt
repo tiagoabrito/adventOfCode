2931e4094cdddb38c2a761a78e20ef5806d18ba5
Tiago Brito (CTW) <tiago.brito@ctw.bmwgroup.com>
2024-12-03 10:32:00 +0000

    AOC: 2024 day 3

--- src/year2024/day03/Day.kt
+++ src/year2024/day03/Day.kt
@@ -5,22 +5,23 @@ import year2024.solveIt
 fun main() {
     val day = "03"
 
-    val expectedTest1 = 0L
-    val expectedTest2 = 0L
+    val expectedTest1 = 161L
+    val expectedTest2 = 48L
+
+    fun getNumber(it: MatchResult, idx: Int):Long = it.groups[idx]?.value?.toLong()?:0
+    fun removeDisabled(it: String) = it.replace("don't\\(\\).*?do\\(\\)".toRegex(), "")
 
 
     fun part1(input: List<String>): Long {
-        return 0
+        return input
+            .flatMap { Regex("mul\\((\\d{1,3}),(\\d{1,3})\\)").findAll(it) }
+            .sumOf { getNumber(it, 1) * getNumber(it, 2) }
     }
-
 
     fun part2(input: List<String>): Long {
-        return 0
-
-
+        val enabledMultiplications = input.map { removeDisabled(it) }
+        return part1(enabledMultiplications)
     }
 
-
-    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test2")
-
+    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test")
 }
