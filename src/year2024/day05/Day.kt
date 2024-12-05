4e36ce95a7dd207fd461bd9b376a0b588f9d8bdf
Tiago Brito (CTW) <tiago.brito@ctw.bmwgroup.com>
2024-12-05 11:07:07 +0000

    AOC: 2024 day 5

--- src/year2024/day05/Day.kt
+++ src/year2024/day05/Day.kt
@@ -1,28 +1,50 @@
 package year2024.day05
 
-import java.util.stream.LongStream
 import year2024.solveIt
 
 
 fun main() {
     val day = "05"
 
-    val expectedTest1 = 0L
-    val expectedTest2 = 0L
+    val expectedTest1 = 143
+    val expectedTest2 = 123
 
 
-    fun part1(input: List<String>): Long {
-        return 0
+    fun getRules(input: List<String>) = input.takeWhile { it.isNotEmpty() }.map { it.split("|") }.groupBy({ it[1] }, { it[0] })
+
+    fun getPages(input: List<String>) = input.asSequence()
+        .dropWhile { it.isNotEmpty() }.drop(1)
+        .map { it.split(",") }
+
+    fun part1(input: List<String>): Int {
+        val rules = getRules(input)
+
+        return getPages(input)
+            .filter { it.zipWithNext().none { e -> rules[e.first]?.contains(e.second) ?: false } }
+            .sumOf { it[it.size / 2].toInt() }
     }
 
 
-    fun part2(input: List<String>): Long {
-        return 0
+    fun sortIt(pages: List<String>, rules: Map<String, List<String>>): List<String> {
+        return pages
+            .map { page -> page to (rules[page]?.count { pages.contains(it) } ?: 0) }
+            .sortedBy { it.second }
+            .map { it.first }
+    }
+
+    fun part2(input: List<String>): Int {
+        val rules = getRules(input)
+
+        val unOrdered = getPages(input)
+            .filter { it.zipWithNext().any { e -> rules[e.first]?.contains(e.second) ?: false } }
 
 
+        return unOrdered
+            .map { sortIt(it, rules) }
+            .sumOf { it[it.size / 2].toInt() }
     }
 
 
-    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test2")
+    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test")
 
 }
