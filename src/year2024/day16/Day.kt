1dc3388730ad4882a7a8590247c2f36affc0d008
Tiago Brito (CTW) <tiago.brito@ctw.bmwgroup.com>
2024-12-20 02:11:54 +0000

    AOC: 2024 day 16

--- src/year2024/day16/Day.kt
+++ src/year2024/day16/Day.kt
@@ -1,107 +1,101 @@
 package year2024.day16
 
-import java.util.LinkedList
-import java.util.Queue
+import java.util.PriorityQueue
 import year2024.solveIt
 
 enum class Direction {
-    RIGHT, LEFT, UP, DOWN
+    WEST, NORTH, EAST, SOUTH
 }
 
-data class Position(val line: Int, val column: Int) {
-    fun nav(other: Position): Position = Position(line + other.line, column + other.column)
+data class Position(val positions: List<Pair<Int,Int>>, val direction: Direction, val cost: Int ) {
+    val current = positions.last()
+        get() = field
 
-    fun nav(direction: Direction): Position = when (direction) {
-        Direction.RIGHT -> Position(line, column + 1)
-        Direction.LEFT -> Position(line, column - 1)
-        Direction.UP -> Position(line - 1, column)
-        Direction.DOWN -> Position(line + 1, column)
+    fun walk(): List<Position> = listOf(
+        this.copy(
+            positions = positions + (current.first to current.second - 1), direction = Direction.WEST, cost = this.cost + when (this.direction) {
+                Direction.WEST -> 1
+                Direction.EAST -> 2001
+                Direction.NORTH, Direction.SOUTH -> 1001
+            }
+        ),
+        this.copy(
+            positions + (current.first to current.second + 1), direction = Direction.EAST, cost = this.cost + when (this.direction) {
+                Direction.EAST -> 1
+                Direction.WEST -> 2001
+                Direction.NORTH, Direction.SOUTH -> 1001
+            }
+        ),
+        this.copy(
+            positions + (current.first - 1 to current.second), direction = Direction.NORTH, cost = this.cost + when (this.direction) {
+                Direction.WEST -> 1001
+                Direction.EAST -> 1001
+                Direction.NORTH -> 1
+                Direction.SOUTH -> 2001
+            }
+        ),
+        this.copy(
+            positions + (current.first + 1 to current.second), direction = Direction.SOUTH, cost = this.cost + when (this.direction) {
+                Direction.WEST -> 1001
+                Direction.EAST -> 1001
+                Direction.NORTH -> 2001
+                Direction.SOUTH -> 1
+            }
+        ),
+    ).filter { steps ->
+        this.positions.none { p -> steps.positions.last() == p }
     }
-
-    fun isBounded(maxLine: Int, maxColumn: Int) = line in 0 until maxLine && column in 0 until maxColumn
 }
 
 fun main() {
     val day = "16"
 
-    val expectedTest1 = 46L
-    val expectedTest2 = 51L
-
-    val navigateRIGHT: Map<Char, Sequence<Direction>> = mapOf(
-        '\\' to sequenceOf(Direction.DOWN),
-        '/' to sequenceOf(Direction.UP),
-        '|' to sequenceOf(Direction.UP, Direction.DOWN),
-        '-' to sequenceOf(Direction.RIGHT),
-        '.' to sequenceOf(Direction.RIGHT),
-    )
-
-    val navigateLEFT: Map<Char, Sequence<Direction>> = mapOf(
-        '\\' to sequenceOf(Direction.UP),
-        '/' to sequenceOf(Direction.DOWN),
-        '|' to sequenceOf(Direction.UP, Direction.DOWN),
-        '-' to sequenceOf(Direction.LEFT),
-        '.' to sequenceOf(Direction.LEFT),
-    )
+    val expectedTest1 = 11048
+    val expectedTest2 = 64
 
 
-    val navigateUP: Map<Char, Sequence<Direction>> = mapOf(
-        '\\' to sequenceOf(Direction.LEFT),
-        '/' to sequenceOf(Direction.RIGHT),
-        '-' to sequenceOf(Direction.LEFT, Direction.RIGHT),
-        '|' to sequenceOf(Direction.UP),
-        '.' to sequenceOf(Direction.UP),
-    )
+    fun getPosition(input: List<String>, c: Char) = input.mapIndexed { l, line -> l to line.indexOf(c) }.first { it.second != -1 }
 
-    val navigateDOWN: Map<Char, Sequence<Direction>> = mapOf(
-        '\\' to sequenceOf(Direction.RIGHT),
-        '/' to sequenceOf(Direction.LEFT),
-        '-' to sequenceOf(Direction.LEFT, Direction.RIGHT),
-        '|' to sequenceOf(Direction.DOWN),
-        '.' to sequenceOf(Direction.DOWN),
-    )
+    fun getSolutions(input: List<String>): MutableSet<Position> {
+        val start = getPosition(input, 'S')
+        val end = getPosition(input, 'E')
 
-    val navMap = mapOf(
-        Direction.RIGHT to navigateRIGHT, Direction.LEFT to navigateLEFT, Direction.UP to navigateUP, Direction.DOWN to navigateDOWN
-    )
+        val queue = PriorityQueue<Position>(compareBy { it.cost })
+        queue.add(Position(listOf(start), Direction.EAST, 0))
 
+        val visitedPositions = mutableMapOf<Pair<Pair<Int, Int>, Direction>, Int>()
 
-    fun navigate(map: List<String>, p: Position, direction: Direction): Sequence<Pair<Position, Direction>> {
-        val current = map[p.line][p.column]
-        val sequence = navMap[direction]!![current]!!
-        return sequence.map { p.nav(it) to it }.filter { it.first.isBounded(map.size, map[0].length) }
-    }
-
-
-    fun getLightened(input: List<String>, start: Position, startDirection: Direction): Long {
-        val navigatedMap = mapOf(
-            Direction.RIGHT to mutableSetOf<Position>(),
-            Direction.LEFT to mutableSetOf(),
-            Direction.UP to mutableSetOf(),
-            Direction.DOWN to mutableSetOf()
-        )
-
-        val toNavigate: Queue<Pair<Position, Direction>> = LinkedList()
-        toNavigate.add(start to startDirection)
-        while (toNavigate.isNotEmpty()) {
-            val (position, direction) = toNavigate.remove()
-            if (navigatedMap[direction]?.add(position) == true) {
-                navigate(input, position, direction).forEach { toNavigate.add(it) }
+        val solutions = mutableSetOf<Position>()
+        while (queue.isNotEmpty()) {
+            val poll = queue.poll()
+            if (poll.current == end) {
+                solutions.add(poll)
             }
+            if (poll.cost > (solutions.firstOrNull()?.cost ?: Int.MAX_VALUE)) {
+                break
+            }
+            poll.walk().filter { input[it.current.first][it.current.second] != '#' }
+                .filter { next ->
+                    val currentCost = visitedPositions.getOrDefault(Pair(next.current, next.direction), Int.MAX_VALUE)
+                    if (next.cost <= currentCost) {
+                        visitedPositions[Pair(next.current, next.direction)] = next.cost // Update cost
+                        true
+                    } else {
+                        false
+                    }
+                }
+                .forEach { queue.add(it) }
         }
-
-        return navigatedMap.values.flatten().distinct().count().toLong()
+        return solutions
     }
 
-    fun part1(input: List<String>): Long {
-        return getLightened(input, Position(0, 0), Direction.RIGHT)
+    fun part1(input: List<String>): Int {
+        val solutions1 = getSolutions(input)
+        return solutions1.first().cost
     }
 
-    fun part2(input: List<String>): Long {
-       return listOf(input.indices.map { i -> Position(i, 0) to Direction.RIGHT },
-            input.indices.map { i -> Position(i, input[0].length - 1) to Direction.LEFT },
-            input[0].indices.map { i -> Position(0, i) to Direction.DOWN },
-            input[0].indices.map { i -> Position(input.size - 1, i) to Direction.UP }).flatten()
-            .maxOfOrNull { getLightened(input, it.first, it.second) }?:0;
+    fun part2(input: List<String>): Int {
+       return getSolutions(input).flatMap { it.positions }.toSet().size
     }
 
     solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test")
