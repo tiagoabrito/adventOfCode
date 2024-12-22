7ad68cf9f10f8a9047185476d1a38d210dfd5d83
Tiago Brito (CTW) <tiago.brito@ctw.bmwgroup.com>
2024-12-22 12:48:58 +0000

    AOC: 2024 day 19

--- src/year2024/day19/Day.kt
+++ src/year2024/day19/Day.kt
@@ -1,28 +1,48 @@
 package year2024.day19
 
-import java.util.LinkedList
-import java.util.Queue
 import year2024.solveIt
 
 
 fun main() {
     val day = "19"
-    val expectedTest1 = 0L
-    val expectedTest2 = 0L
+    val expectedTest1 = 6
+    val expectedTest2 = 16L
 
 
-    fun part1(input: List<String>): Long {
-        return 0
+    fun countPatterns(towel: String, patterns: List<String>): Long {
+        val possibleCombinationsToReachSolution = LongArray(towel.length + 1)
+        possibleCombinationsToReachSolution[0] = 1;
+
+        return (0..towel.length).fold(possibleCombinationsToReachSolution) { dp, currIdx ->
+            if (dp[currIdx] > 0) {
+                patterns
+                    .filter { currIdx + it.length <= towel.length }
+                    .filter { towel.substring(currIdx, currIdx + it.length) == it }
+                    .forEach {
+                        dp[currIdx + it.length] += dp[currIdx]
+                    }
+            }
+            dp
+        }.last()
     }
 
+    fun getPossiblePatterns(input: List<String>): List<Long> {
+        val patterns = input[0].split(", ").toList()
+        val towels = input.drop(2)
+
+        val possiblePatterns = towels.map { countPatterns(it, patterns) }
+        return possiblePatterns
+    }
+
+    fun part1(input: List<String>): Int {
+        return getPossiblePatterns(input).count { it > 0 }
+    }
 
     fun part2(input: List<String>): Long {
-        return 0
-
-
+        return getPossiblePatterns(input).sum()
     }
 
 
-    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test2")
+    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test")
 
 }
