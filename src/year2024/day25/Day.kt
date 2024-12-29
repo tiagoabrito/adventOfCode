fa63ef5e9a92175e1096febe46889fbf6c3bcd2d
Tiago Brito (CTW) <tiago.brito@ctw.bmwgroup.com>
2024-12-29 00:49:11 +0000

    AOC: 2024 day 25 part1

--- src/year2024/day25/Day.kt
+++ src/year2024/day25/Day.kt
@@ -5,12 +5,36 @@ import year2024.solveIt
 
 fun main() {
     val day = "25"
-    val expectedTest1 = 0L
+    val expectedTest1 = 3
     val expectedTest2 = 0L
 
 
-    fun part1(input: List<String>): Long {
-        return 0
+    fun countChar(key: List<String>, toCount: Char): List<Int> {
+        val map = key[0].indices.map { idx -> key.count { it[idx] == toCount } }
+        return map
+    }
+
+    fun getSchematics(input: List<String>) = input.chunked(8).map { it.filter { it.isNotEmpty() } }
+
+    fun getKeys(input: List<String>) = getSchematics(input).mapNotNull {
+        if (it[0][0] == '.') {
+            countChar(it, '.').map { d -> it.size - d - 1 }
+        } else null
+    }
+
+    fun getLocks(input: List<String>) = getSchematics(input).mapNotNull {
+        if (it[0][0] == '#') {
+            countChar(it, '#').map { d -> d - 1 }
+        } else null
+    }
+
+    fun part1(input: List<String>): Int {
+        val keys = getKeys(input)
+        val locks = getLocks(input)
+
+
+        val flatMap = locks.flatMap { l -> keys.map { l to it } }
+        return flatMap.count { it.first.indices.all { idx -> (6 - it.first[idx]) > it.second[idx] } }
     }
 
 
@@ -21,6 +45,6 @@ fun main() {
     }
 
 
-    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test2")
+    solveIt(day, ::part1, expectedTest1, ::part2, expectedTest2, "test")
 
 }
